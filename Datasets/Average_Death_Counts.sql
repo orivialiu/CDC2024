66f1fa0b298593d0df7c19879b17a644ba49d2e7
Olivia Liu <oaliu@unc.edu>
2024-09-28 22:47:13 -0400

    Create Average_Death_Counts.sql

---
+++ Datasets/Average_Death_Counts.sql
@@ -0,0 +1,21 @@
+SELECT
+  Jurisdiction,
+  ROUND(AVG(COVID_19_Deaths)) AS Mean_Covid,
+  ROUND(AVG(Total_Deaths)) AS Mean_Total,
+  ROUND(AVG(Pneumonia_Deaths)) AS Mean_Pneumonia,
+  ROUND(AVG(Influenza_Deaths)) AS Mean_Influenza,
+  ROUND(AVG(Pneumonia_or_Influenza)) AS Mean_Pneumonia_Influenza,
+  ROUND(AVG(Pneumonia_Influenza_or_COVID_19_Deaths)) AS Mean_Pneumonia_Influenza_Covid
+FROM
+  Health_Science_Dataset b
+WHERE
+  b.Jurisdiction != 'United States'
+  AND b.Jurisdiction NOT LIKE 'HHS%'
+  AND COVID_19_Deaths IS NOT NULL
+  AND Total_Deaths IS NOT NULL
+  AND Pneumonia_Deaths IS NOT NULL
+  AND Influenza_Deaths IS NOT NULL
+  AND Pneumonia_Influenza_or_COVID_19_Deaths IS NOT NULL
+  AND Pneumonia_or_Influenza IS NOT NULL
+GROUP BY
+  Jurisdiction;
